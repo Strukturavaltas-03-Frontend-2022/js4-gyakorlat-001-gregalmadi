0cda762183a1c6f64893fa39ac4929656cfddbf6
Kawi <kawiwinter@gmail.com>
2022-09-03 12:08:31 +0200

    all tests passed

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,16 @@
+const convertToUppercase = (array) => {
+  return new Promise((resolve, reject) => {
+    let convertedArray = [];
+    try {
+      array.forEach((el) => {
+        convertedArray.push(el.toUpperCase());
+      });
+      resolve(convertedArray);
+    } catch (error) {
+      const customError = "Error: Not all items in the array are strings!";
+      reject(new Error(customError));
+    }
+  });
+};
+
 export default convertToUppercase;
